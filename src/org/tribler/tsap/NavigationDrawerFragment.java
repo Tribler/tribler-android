72ea20afa8998d382672d3b3fdd5675136c5f32c
Niels Spruit <niellus16@hotmail.com>
2014-05-02 11:57:50 +0200

    Changed example action button into search button and included search icons

--- src/org/tribler/tsap/NavigationDrawerFragment.java
+++ src/org/tribler/tsap/NavigationDrawerFragment.java
@@ -248,8 +248,8 @@ public class NavigationDrawerFragment extends Fragment {
             return true;
         }
 
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
+        if (item.getItemId() == R.id.action_search) {
+            Toast.makeText(getActivity(), "Search action.", Toast.LENGTH_SHORT).show();
             return true;
         }
 

35c74f7de1acccc8107d4d5eded698556c2f3aa8
Niels Spruit <niellus16@hotmail.com>
2014-05-02 11:25:17 +0200

    Removed unused imports

--- src/org/tribler/tsap/MainActivity.java
+++ src/org/tribler/tsap/MainActivity.java
@@ -1,20 +1,16 @@
 package org.tribler.tsap;
 
-import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends Activity

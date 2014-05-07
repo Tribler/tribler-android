1dcb7bf0fcc1c9d714faa545c3049d019c3e594c
Niels Spruit <niellus16@hotmail.com>
2014-05-07 09:02:47 +0200

    Added initial video info fragment files

---
+++ tsap/src/org/tribler/tsap/VideoInfoFragment.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.tribler.tsap;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author niels
+ *
+ */
+public class VideoInfoFragment extends Fragment {
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		return null;		
+	}
+
+}

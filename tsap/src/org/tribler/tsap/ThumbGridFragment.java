2ea357daa65c7b6b80b4a0266d88e44e45928f09
wtud <ws+tud@dosoftware.nl>
2014-05-07 11:37:20 +0200

    Replace real movies with open/old movies

--- tsap/src/org/tribler/tsap/ThumbGridFragment.java
+++ tsap/src/org/tribler/tsap/ThumbGridFragment.java
@@ -4,17 +4,14 @@ import java.util.ArrayList;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.app.Fragment;
-import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
@@ -35,18 +32,26 @@ public class ThumbGridFragment extends Fragment {
     		
     	GridView gridView = (GridView) v.findViewById(R.id.ThumbsGrid);
         
+    	float s = v.getContext().getResources().getDisplayMetrics().density;
+    	int w = (int)(100 * s);
+    	int h = (int)(150 * s);
+    	
     	//set grid view item
-    	Bitmap bmSintel = BitmapFactory.decodeResource(this.getResources(), R.drawable.sintel);
-    	Bitmap bmAvatar = BitmapFactory.decodeResource(this.getResources(), R.drawable.avatar);
-    	Bitmap bmLOTR = BitmapFactory.decodeResource(this.getResources(), R.drawable.lotr);
-    	Bitmap bmTuckerdale = BitmapFactory.decodeResource(this.getResources(), R.drawable.tuckerdale);
+    	Bitmap bmSintel = decodeSampledBitmapFromResource(this.getResources(), R.drawable.sintel, w, h);
+    	Bitmap bmTOS = decodeSampledBitmapFromResource(this.getResources(), R.drawable.tos, w, h);
+    	//Bitmap bmBBB = decodeSampledBitmapFromResource(this.getResources(), R.drawable.bigbuckbunny, w, h);
+    	Bitmap bmKingkong = decodeSampledBitmapFromResource(this.getResources(), R.drawable.kingkong, w, h);
+    	Bitmap bmLastman = decodeSampledBitmapFromResource(this.getResources(), R.drawable.lastman, w, h);
+    	Bitmap bm50ft = decodeSampledBitmapFromResource(this.getResources(), R.drawable.fiftyft, w, h);
 
     	for(int i = 0; i < 11; i++)
     	{
     		gridArray.add(new ThumbItem("Sintel", bmSintel, ThumbItem.TORRENT_HEALTH.GREEN, 500));
-    		gridArray.add(new ThumbItem("Avatar", bmAvatar, ThumbItem.TORRENT_HEALTH.RED, 423));
-    		gridArray.add(new ThumbItem("Lord of the Rings", bmLOTR, ThumbItem.TORRENT_HEALTH.YELLOW, 4321));
-    		gridArray.add(new ThumbItem("Tucker and Dale vs. Evil", bmTuckerdale, ThumbItem.TORRENT_HEALTH.UNKNOWN, 12353));
+    		gridArray.add(new ThumbItem("Tears of Steal", bmTOS, ThumbItem.TORRENT_HEALTH.RED, 423));
+    		//gridArray.add(new ThumbItem("Big Buck Bunny", bmBBB, ThumbItem.TORRENT_HEALTH.YELLOW, 4321));
+    		gridArray.add(new ThumbItem("King Kong", bmKingkong, ThumbItem.TORRENT_HEALTH.UNKNOWN, 12353));
+    		gridArray.add(new ThumbItem("To The Last Man", bmLastman, ThumbItem.TORRENT_HEALTH.UNKNOWN, 12353));
+    		gridArray.add(new ThumbItem("Attack of the 50 ft woman", bm50ft, ThumbItem.TORRENT_HEALTH.UNKNOWN, 12353));
     	}
 		
 		ThumbAdapter customThumbs = new ThumbAdapter(container.getContext(), R.layout.thumb_item, gridArray);
@@ -58,9 +63,47 @@ public class ThumbGridFragment extends Fragment {
 				Toast.makeText(v.getContext() , clickItem.getTitle() + " (id: " + id + ")", Toast.LENGTH_SHORT).show();
 			}
 		});
-
        
         return v;
     }
+    
+    private static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+            int reqWidth, int reqHeight) {
 
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+    
+    private static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    // Raw height and width of image
+    final int height = options.outHeight;
+    final int width = options.outWidth;
+    int inSampleSize = 1;
+
+    if (height > reqHeight || width > reqWidth) {
+
+        final int halfHeight = height / 2;
+        final int halfWidth = width / 2;
+
+        // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+        // height and width larger than the requested height and width.
+        while ((halfHeight / inSampleSize) > reqHeight
+                && (halfWidth / inSampleSize) > reqWidth) {
+            inSampleSize *= 2;
+        }
+    }
+
+    return inSampleSize;
+}    
+    
 }

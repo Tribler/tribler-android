36ce7b665cbb85883ffedce723daa2e9997149e8
Niels Spruit <niellus16@hotmail.com>
2014-05-08 09:34:02 +0200

    Removed unused import

--- tsap/src/org/tribler/tsap/MainActivity.java
+++ tsap/src/org/tribler/tsap/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;

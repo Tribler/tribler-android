0e4f4dc4c5a9cca2cbcfe221c0fbca9de06c6134
wtud <ws+tud@dosoftware.nl>
2014-05-14 12:10:02 +0200

    Add -c flag to clear log before starting pidcat

--- tribler-prototype/lbuild.sh
+++ tribler-prototype/lbuild.sh
@@ -9,4 +9,5 @@ source /home/user/Documents/Projects/bep/setenv_x86.sh &&
 ./build.sh -p /home/user/Documents/Projects/bep/p4a-at3 &&
 adb uninstall org.tsap.tribler.full &&
 adb install app/a-TSAPTribler-0.9-debug.apk &&
-python pidcat/pidcat.py org.tsap.tribler.full
+echo Listening for logcat output.. &&
+python pidcat/pidcat.py org.tsap.tribler.full -c

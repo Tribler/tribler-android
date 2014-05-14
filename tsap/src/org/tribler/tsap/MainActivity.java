db81719427afba5086d6e1ee7b0a9e7fcca2a40e
Niels Spruit <niellus16@hotmail.com>
2014-05-14 10:16:56 +0200

    Renamed menu resources into more descriptive names and removed search action from navigation drawer

--- tsap/src/org/tribler/tsap/MainActivity.java
+++ tsap/src/org/tribler/tsap/MainActivity.java
@@ -83,7 +83,7 @@ public class MainActivity extends Activity
 			// Only show items in the action bar relevant to this screen
 			// if the drawer is not showing. Otherwise, let the drawer
 			// decide what to show in the action bar.
-			getMenuInflater().inflate(R.menu.main, menu);
+			getMenuInflater().inflate(R.menu.main_activity, menu);
 			restoreActionBar();
 			
 			MenuItem mSearchMenuItem = menu.findItem(R.id.action_search);

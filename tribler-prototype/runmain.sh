2f36cc97de02ee14adb395f02969139174c54789
wtud <ws+tud@dosoftware.nl>
2014-05-15 13:14:44 +0200

    Rename runmain->tmprunmain

--- tribler-prototype/runmain.sh
+++ tribler-prototype/runmain.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir -p runmain
+rm -r tmprunmain
+mkdir -p tmprunmain
 
-pushd runmain
-rm -r venv-tsap Tribler swift
+pushd tmprunmain
 
 virtualenv --system-site-packages venv-tsap
 . venv-tsap/bin/activate
@@ -11,10 +11,10 @@ pip install ../Tribler-0.1.0.tar.gz
 #ln -s ../tribler/Tribler .
 popd
 
-cp tsap/* runmain/
-cp tribler/swift runmain/
+cp tsap/* tmprunmain/
+cp tribler/swift tmprunmain/
 
-pushd runmain
+pushd tmprunmain
 
 # A running swift fucks with dispersy, even when you disable swift(?)
 pkill swift

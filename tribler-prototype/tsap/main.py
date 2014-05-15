7bf2779c74a4427c08ae85c002de40e5f48ef6c7
wtud <ws+tud@dosoftware.nl>
2014-05-15 17:34:48 +0200

    Moving main.py code towards a class

--- tribler-prototype/tsap/main.py
+++ tribler-prototype/tsap/main.py
@@ -35,37 +35,76 @@ _logger = logging.getLogger(__name__)
 _logger.error("THIS IS AN ERROR")
 print 'os.getcwd(): %s' % os.getcwd()
 
-def define_communities():
-    #from Tribler.community.search.community import SearchCommunity
-    from Tribler.community.allchannel.community import AllChannelCommunity
-    from Tribler.community.channel.community import ChannelCommunity
-    from Tribler.community.channel.preview import PreviewChannelCommunity
-    from Tribler.community.metadata.community import MetadataCommunity
+class RunApp():
 
-    _logger.info("tribler: Preparing communities...")
-    now = time()
+    dispersy = None
+    config = None
 
-    # must be called on the Dispersy thread
-    #dispersy.define_auto_load(SearchCommunity, s.dispersy_member, load=True)
-    dispersy.define_auto_load(AllChannelCommunity, s.dispersy_member, load=True)
+    def __init__(self):
+        config = SessionStartupConfig()
 
-    # load metadata community
-    dispersy.define_auto_load(MetadataCommunity, s.dispersy_member, load=True)
+        _logger.info("Set tribler_sate_dir to %s" % os.environ['TRIBLER_STATE_DIR'])
+        config.set_state_dir(os.environ['TRIBLER_STATE_DIR'])
+        config.set_torrent_checking(False)
+        config.set_multicast_local_peer_discovery(False)
+        config.set_megacache(False)
+        #config.set_dispersy(False)
+        #config.set_swift_proc(False)
+        config.set_mainline_dht(False)
+        config.set_torrent_collecting(False)
+        config.set_libtorrent(False)
+        config.set_dht_torrent_collecting(False)
+        config.set_videoplayer(False)
 
-    # 17/07/13 Boudewijn: the missing-member message send by the BarterCommunity on the swift port is crashing
-    # 6.1 clients.  We will disable the BarterCommunity for version 6.2, giving people some time to upgrade
-    # their version before enabling it again.
-    # if swift_process:
-    #     dispersy.define_auto_load(BarterCommunity,
-    #                               s.dispersy_member,
-    #                               (swift_process,),
-    #                               load=True)
+        s = Session(config)
+        s.start()
 
-    dispersy.define_auto_load(ChannelCommunity, s.dispersy_member, load=True)
-    dispersy.define_auto_load(PreviewChannelCommunity, s.dispersy_member)
+        dispersy = s.get_dispersy_instance()
 
-    diff = time() - now
-    _logger.info("tribler: communities are ready in %.2f seconds", diff)
+        def define_communities():
+            _logger.error("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+            from Tribler.community.search.community import SearchCommunity
+            from Tribler.community.allchannel.community import AllChannelCommunity
+            from Tribler.community.channel.community import ChannelCommunity
+            from Tribler.community.channel.preview import PreviewChannelCommunity
+            from Tribler.community.metadata.community import MetadataCommunity
+
+            _logger.info("tribler: Preparing communities...")
+            now = time()
+
+            # must be called on the Dispersy thread
+            #comm = dispersy.define_auto_load(SearchCommunity, s.dispersy_member, load=True)
+            _logger.info("Loaded dispersy communities: %s" % comm)
+            comm = dispersy.define_auto_load(AllChannelCommunity, s.dispersy_member, load=True)
+            _logger.info("Loaded dispersy communities: %s" % comm)
+
+            # load metadata community
+            #comm = dispersy.define_auto_load(MetadataCommunity, s.dispersy_member, load=True)
+            _logger.info("Loaded dispersy communities: %s" % comm)
+
+            # 17/07/13 Boudewijn: the missing-member message send by the BarterCommunity on the swift port is crashing
+            # 6.1 clients.  We will disable the BarterCommunity for version 6.2, giving people some time to upgrade
+            # their version before enabling it again.
+            # if swift_process:
+            #     dispersy.define_auto_load(BarterCommunity,
+            #                               s.dispersy_member,
+            #                               (swift_process,),
+            #                               load=True)
+
+            #comm = dispersy.define_auto_load(ChannelCommunity, s.dispersy_member, load=True)
+            _logger.info("Loaded dispersy communities: %s" % comm)
+            #comm = dispersy.define_auto_load(PreviewChannelCommunity, s.dispersy_member)
+            _logger.info("Loaded dispersy communities: %s" % comm)
+
+            diff = time() - now
+            _logger.info("tribler: communities are ready in %.2f seconds", diff)
+
+        dispersy.callback.call(define_communities)
+
+        print 'libTribler session started!'
+
+
 
 if __name__ == '__main__':
     # Prepare swift binary on Android
@@ -82,26 +121,4 @@ if __name__ == '__main__':
             shutil.copy2(swift_path_source, swift_path_dest)
             os.chmod(swift_path_dest, 0777)
 
-    config = SessionStartupConfig()
-
-    _logger.info("Set tribler_sate_dir to %s" % os.environ['TRIBLER_STATE_DIR'])
-    config.set_state_dir(os.environ['TRIBLER_STATE_DIR'])
-    config.set_torrent_checking(False)
-    config.set_multicast_local_peer_discovery(False)
-    config.set_megacache(False)
-    #config.set_dispersy(False)
-    #config.set_swift_proc(False)
-    config.set_mainline_dht(False)
-    config.set_torrent_collecting(False)
-    config.set_libtorrent(False)
-    config.set_dht_torrent_collecting(False)
-    config.set_videoplayer(False)
-
-    s = Session(config)
-    s.start()
-
-    dispersy = s.get_dispersy_instance()
-    dispersy.callback.call(define_communities)
-    #s._logger.setLevel(logging.DEBUG)
-
-    print 'libTribler session started!'
+    app = RunApp()

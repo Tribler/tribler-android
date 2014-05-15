5cbc47309ee3682576aa12251777b85dad2a6a0c
wtud <ws+tud@dosoftware.nl>
2014-05-15 10:08:30 +0200

    Add .md5 file when uploading new Tribler pkg

--- tribler-prototype/mktriblerpkg.sh
+++ tribler-prototype/mktriblerpkg.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-echo "Remove old Tribler code"
+echo "Remove old Tribler code.."
 rm -r Tribler-0.1.0/Tribler
 
-echo "Copy new Tribler code"
+echo "Copy new Tribler code.."
 cp -r tribler/Tribler Tribler-0.1.0/
 
-echo "Remove pyc and pyo files"
+echo "Remove pyc and pyo files.."
 pushd Tribler-0.1.0/Tribler
 find -name '*.pyc' -exec rm {} \;
 find -name '*.pyo' -exec rm {} \;
 popd
 
-echo "Make tarball"
+echo "Make tarball.."
 tar cvzf Tribler-0.1.0.tar.gz Tribler-0.1.0
 
-echo "MD5SUM: "
-md5sum Tribler-0.1.0.tar.gz
+echo "md5sum:"
+md5sum Tribler-0.1.0.tar.gz > Tribler-0.1.0.tar.gz.md5
+cat Tribler-0.1.0.tar.gz.md5
 
-echo "Copy to server"
-scp Tribler-0.1.0.tar.gz fr:/var/www/default/tsap/
+echo "Copy to server.."
+scp Tribler-0.1.0.tar.gz Tribler-0.1.0.tar.gz.md5 fr:/var/www/default/tsap/

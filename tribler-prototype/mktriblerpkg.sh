470f5cc899d5dd403ed74d41cfc392d240c4187b
wtud <ws+tud@dosoftware.nl>
2014-05-16 10:43:31 +0200

    Automatically replace the md5 checksum in the tribler recipe

--- tribler-prototype/mktriblerpkg.sh
+++ tribler-prototype/mktriblerpkg.sh
@@ -15,8 +15,19 @@ echo "Make tarball.."
 tar cvzf Tribler-0.1.0.tar.gz Tribler-0.1.0
 
 echo "md5sum:"
-md5sum Tribler-0.1.0.tar.gz > Tribler-0.1.0.tar.gz.md5
+MD5SUM=`md5sum Tribler-0.1.0.tar.gz`
+echo $MD5SUM > Tribler-0.1.0.tar.gz.md5
 cat Tribler-0.1.0.tar.gz.md5
 
 echo "Copy to server.."
 scp Tribler-0.1.0.tar.gz Tribler-0.1.0.tar.gz.md5 fr:/var/www/default/tsap/
+
+if [ -f "${P4A_PATH}/recipes/Tribler/recipe.sh" ]
+then
+	echo "Replacing MD5SUM in ${P4A_PATH}/recipes/Tribler/recipe.sh"
+	pushd "${P4A_PATH}/recipes/Tribler" > /dev/null
+	MD5SUM=`echo -e $MD5SUM | cut -d' ' -f1`
+	mv recipe.sh recipe.sh.orig
+	sed -e "s/^MD5_Tribler=.*/MD5_Tribler=$MD5SUM/" recipe.sh.orig > recipe.sh
+	popd > /dev/null
+fi

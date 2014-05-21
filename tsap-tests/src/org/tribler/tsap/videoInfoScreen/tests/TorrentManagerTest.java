dea53cfaac918bd0a717214657a0cf705c50c6fc
Niels Spruit <niellus16@hotmail.com>
2014-05-21 16:40:12 +0200

    Added tests for the new methods in TorrentManager

--- tsap-tests/src/org/tribler/tsap/videoInfoScreen/tests/TorrentManagerTest.java
+++ tsap-tests/src/org/tribler/tsap/videoInfoScreen/tests/TorrentManagerTest.java
@@ -1,8 +1,12 @@
 package org.tribler.tsap.videoInfoScreen.tests;
 
-import org.tribler.tsap.videoInfoScreen.TorrentManager;
+import java.util.ArrayList;
 
 import junit.framework.TestCase;
+
+import org.tribler.tsap.thumbgrid.ThumbItem;
+import org.tribler.tsap.videoInfoScreen.Torrent;
+import org.tribler.tsap.videoInfoScreen.TorrentManager;
 
 /**
  * Unit tests for the TorrentManager class
@@ -42,6 +46,26 @@ public class TorrentManagerTest extends TestCase {
 		assertEquals("getNumberOfTorrents() incorrect", 10,
 				mTorrentManager.getNumberOfTorrents());
 	}
+	
+	/**
+	 * Test whether the torrent manager returns the correct torrent
+	 */
+	public void testGetTorrentById()
+	{
+		Torrent tor = mTorrentManager.getTorrentById(0);
+		assertNotNull("tor is null", tor);
+		assertEquals("getTorrentById(0) incorrect","Sintel",tor.getName());
+	}
+	
+	/**
+	 * Test whether the torrent manager returns null when a non-existing torrent
+	 * is requested
+	 */
+	public void testGetTorrentByIdNull()
+	{
+		Torrent tor = mTorrentManager.getTorrentById(mTorrentManager.getNumberOfTorrents());
+		assertNull("tor is not null", tor);
+	}
 
 	/**
 	 * Tests whether the TorrentManager.containsTorrentWithID() method works
@@ -56,5 +80,15 @@ public class TorrentManagerTest extends TestCase {
 				mTorrentManager.containsTorrentWithID(2));
 		assertFalse("containsTorrentWithID(getNumberOfTorrents()) incorrect",
 				mTorrentManager.containsTorrentWithID(mTorrentManager.getNumberOfTorrents()));
+	}
+	
+	/**
+	 * Tests whether the TorrentManger.getThumbItem method functions correctly
+	 */
+	public void testGetThumbItems()
+	{
+		ArrayList<ThumbItem> thumbs = mTorrentManager.getThumbItems();
+		assertNotNull("thumbs is null", thumbs);
+		assertEquals("size of thumbs incorrect", mTorrentManager.getNumberOfTorrents(), thumbs.size());
 	}
 }

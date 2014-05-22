10adcf61d290b40d50722bb79d34381a3d30938f
Niels Spruit <niellus16@hotmail.com>
2014-05-22 10:24:48 +0200

    Added documentation to ChannelActivity class

--- tsap/src/org/tribler/tsap/ChannelActivity.java
+++ tsap/src/org/tribler/tsap/ChannelActivity.java
@@ -4,15 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
  
+/**
+ * Activity that is started when a channel within the channel list fragment is clicked
+ * @author Dirk Schut
+ */
 public class ChannelActivity extends Activity{
+	
+	/**
+	 * OnCreate callback: sets the content view and sets the title to the values passed in with the intent
+	 */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.channel_activity_view);         
         Intent i = getIntent();
-        // getting attached izntent data
         String product = i.getStringExtra("product");
-        // displaying selected product name
         setTitle(product);
     }
 }

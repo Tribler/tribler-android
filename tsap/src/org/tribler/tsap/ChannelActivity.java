e7f9c79d287ff0d9e8cf2354cc412993c172484f
Niels Spruit <niellus16@hotmail.com>
2014-05-22 09:17:13 +0200

    Removed unused variable and import

--- tsap/src/org/tribler/tsap/ChannelActivity.java
+++ tsap/src/org/tribler/tsap/ChannelActivity.java
@@ -3,16 +3,12 @@ package org.tribler.tsap;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
  
 public class ChannelActivity extends Activity{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.channel_activity_view);
-         
-        TextView txtProduct = (TextView) findViewById(R.id.product_label);
-         
+        this.setContentView(R.layout.channel_activity_view);         
         Intent i = getIntent();
         // getting attached izntent data
         String product = i.getStringExtra("product");

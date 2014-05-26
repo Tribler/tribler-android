c0d6df123bc56e6480618fc1a0807c199c721a33
Niels Spruit <niellus16@hotmail.com>
2014-05-26 13:06:41 +0200

    Modified testDrawerClosed (see comment on issue)

--- tsap-tests/src/org/tribler/tsap/tests/NavigationDrawerFragmentTest.java
+++ tsap-tests/src/org/tribler/tsap/tests/NavigationDrawerFragmentTest.java
@@ -4,17 +4,22 @@ import org.tribler.tsap.MainActivity;
 import org.tribler.tsap.NavigationDrawerFragment;
 import org.tribler.tsap.R;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
 import android.test.ActivityInstrumentationTestCase2;
 
 /**
  * Functional test cases of the Navigation Drawer fragment
+ * 
  * @author Niels Spruit
  */
 public class NavigationDrawerFragmentTest extends
 		ActivityInstrumentationTestCase2<MainActivity> {
-	
+
 	private NavigationDrawerFragment mNavDrawFrag;
-	
+	private MainActivity mMainActivity;
+
 	public NavigationDrawerFragmentTest() {
 		super(MainActivity.class);
 	}
@@ -22,10 +27,11 @@ public class NavigationDrawerFragmentTest extends
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		MainActivity mMainActivity = getActivity();
-		mNavDrawFrag = (NavigationDrawerFragment) mMainActivity.getFragmentManager().findFragmentById(R.id.navigation_drawer);
+		mMainActivity = getActivity();
+		mNavDrawFrag = (NavigationDrawerFragment) mMainActivity
+				.getFragmentManager().findFragmentById(R.id.navigation_drawer);
 	}
-	
+
 	/**
 	 * Tests whether the test fixture is set up correctly
 	 */
@@ -34,10 +40,15 @@ public class NavigationDrawerFragmentTest extends
 	}
 
 	/**
-	 * Test whether the navigation drawer is closed
+	 * Test whether the navigation drawer is open the first time the app is
+	 * started and that the user hasn't learned the navigation drawer yet
 	 */
-	public void testDrawerClosed()
-	{
-		assertFalse("Navigation drawer is open",mNavDrawFrag.isDrawerOpen());
+	public void testDrawerSharedPreferenceFirstStartup() {
+		SharedPreferences sp = PreferenceManager
+				.getDefaultSharedPreferences(mMainActivity);
+		assertTrue("Navigation drawer is closed on first startup",
+				mNavDrawFrag.isDrawerOpen());
+		assertFalse("User cannot have learned the drawer on first startup",
+				sp.getBoolean("navigation_drawer_learned", false));
 	}
 }

bef81b76d5d4e9d32cb78793c8fbef652c157395
Niels Spruit <niellus16@hotmail.com>
2014-05-28 11:37:04 +0200

    Added tests for button press events

--- tsap-UItests/src/org/tribler/tsap/UItests/SpecialEventsUiTest.java
+++ tsap-UItests/src/org/tribler/tsap/UItests/SpecialEventsUiTest.java
@@ -1,39 +1,102 @@
 package org.tribler.tsap.UItests;
 
+import javax.crypto.SealedObject;
+
+import android.os.RemoteException;
+
+import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiSelector;
+
 /**
  * UI tests for special events like pressing the home button
+ * 
  * @author Niels Spruit
- *
+ * 
  */
 public class SpecialEventsUiTest extends BasicUiTestCase {
 
-	public void testPressBack()
-	{
-		
+	/**
+	 * Tests whether TSAP in closed and the launcher is opened when the back
+	 * button is pressed
+	 * 
+	 * @throws RemoteException
+	 * @throws UiObjectNotFoundException
+	 */
+	public void testPressBack() throws RemoteException,
+			UiObjectNotFoundException {
+		startTSAP();
+		getUiDevice().pressBack();
+
+		UiObject tsapValidation = new UiObject(
+				new UiSelector().packageName("org.tribler.tsap"));
+		assertFalse("Able to find TSAP app after pressing back",
+				tsapValidation.exists());
+
+		UiObject launcher = new UiObject(
+				new UiSelector().packageName("com.android.launcher"));
+		assertTrue("Launcher doesn't exist after pressing back",
+				launcher.exists());
 	}
-	
-	public void testPressHome()
-	{
-		
+
+	/**
+	 * Tests whether TSAP in closed and the launcher is opened when the home
+	 * button is pressed
+	 * 
+	 * @throws RemoteException
+	 * @throws UiObjectNotFoundException
+	 */
+	public void testPressHome() throws RemoteException,
+			UiObjectNotFoundException {
+		startTSAP();
+		getUiDevice().pressHome();
+
+		UiObject tsapValidation = new UiObject(
+				new UiSelector().packageName("org.tribler.tsap"));
+		assertFalse("Able to find TSAP app after pressing home",
+				tsapValidation.exists());
+
+		UiObject launcher = new UiObject(
+				new UiSelector().packageName("com.android.launcher"));
+		assertTrue("Launcher doesn't exist after pressing home",
+				launcher.exists());
 	}
-	
-	public void testPressRecentApps()
-	{
+
+	/**
+	 * Tests whether TSAP in closed and the systemUI is opened when the recent apps
+	 * button is pressed and that the recent apps window contains the TSAP app
+	 * 
+	 * @throws RemoteException
+	 * @throws UiObjectNotFoundException
+	 */
+	public void testPressRecentApps() throws RemoteException, UiObjectNotFoundException {
+		startTSAP();
+		getUiDevice().pressRecentApps();
+
+		UiObject tsapValidation = new UiObject(
+				new UiSelector().packageName("org.tribler.tsap"));
+		assertFalse("Able to find TSAP app after pressing recent apps",
+				tsapValidation.exists());
+
+		UiObject systemUI = new UiObject(
+				new UiSelector().packageName("com.android.systemui"));
+		assertTrue("SystemUI doesn't exist after pressing recent apps",
+				systemUI.exists());
 		
+		UiObject tsapItem = systemUI.getChild(new UiSelector().text("TSAP"));
+		assertTrue("TSAP doesn't exist in recent apps", tsapItem.exists());
+		assertTrue("TSAP isn't enabled in recent apps", tsapItem.isEnabled());
 	}
-	
-	public void testOpenNotifications()
-	{
-		
+
+	public void testOpenNotifications() {
+
 	}
-	
-	public void testOpenQuickSettings()
-	{
-		
+
+	public void testOpenQuickSettings() {
+
 	}
-	
-	public void testSleepWakeUp()
-	{
-		
+
+	public void testSleepWakeUp() {
+
 	}
 }

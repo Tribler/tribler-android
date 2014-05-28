92ebd9d5145af5f67a861762bf2b65d97219b3c3
Niels Spruit <niellus16@hotmail.com>
2014-05-28 11:12:51 +0200

    Created test structure for special events

---
+++ tsap-UItests/src/org/tribler/tsap/UItests/SpecialEventsUiTest.java
@@ -0,0 +1,39 @@
+package org.tribler.tsap.UItests;
+
+/**
+ * UI tests for special events like pressing the home button
+ * @author Niels Spruit
+ *
+ */
+public class SpecialEventsUiTest extends BasicUiTestCase {
+
+	public void testPressBack()
+	{
+		
+	}
+	
+	public void testPressHome()
+	{
+		
+	}
+	
+	public void testPressRecentApps()
+	{
+		
+	}
+	
+	public void testOpenNotifications()
+	{
+		
+	}
+	
+	public void testOpenQuickSettings()
+	{
+		
+	}
+	
+	public void testSleepWakeUp()
+	{
+		
+	}
+}

a556554a856c18a7ff60927d2f8175d02bfc6603
Niels Spruit <niellus16@hotmail.com>
2014-05-28 10:29:54 +0200

    Added tests for video info screen in landscape mode

---
+++ tsap-UItests/src/org/tribler/tsap/UItests/VideoInfoLandscapeUiTest.java
@@ -0,0 +1,25 @@
+package org.tribler.tsap.UItests;
+
+import android.os.RemoteException;
+
+import com.android.uiautomator.core.UiObjectNotFoundException;
+
+/**
+ * UI test cases for the video info screen in landscape mode
+ * 
+ * @author Niels Spruit
+ */
+public class VideoInfoLandscapeUiTest extends VideoInfoUiTest {
+
+	/**
+	 * Launches the TSAP app on the device and sets the orientation to Right
+	 * 
+	 * @throws UiObjectNotFoundException
+	 * @throws RemoteException 
+	 */
+	@Override
+	protected void startTSAP() throws UiObjectNotFoundException, RemoteException {
+		super.startTSAP();
+		getUiDevice().setOrientationRight();
+	}
+}

17aa9d4d311d340911a71c7cd8ba10379430b44a
Niels Spruit <niellus16@hotmail.com>
2014-06-02 12:03:05 +0200

    Added necessary p4a classes

---
+++ UnpackerTest/src/org/tribler/tsap/unpackertest/PythonActivity.java
@@ -0,0 +1,310 @@
+package org.tribler.tsap.unpackertest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+
+public class PythonActivity extends Activity implements Runnable {
+    private static String TAG = "Python";
+
+    // The SDLSurfaceView we contain.
+    public static PythonActivity mActivity = null;
+
+    // Did we launch our thread?
+    private boolean mLaunchedThread = false;
+
+    private ResourceManager resourceManager;
+
+    // The path to the directory containing our external storage.
+    private File externalStorage;
+
+    // The path to the directory containing the game.
+    private File mPath = null;
+
+    boolean _isPaused = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        PythonActivity.mActivity = this;
+
+        resourceManager = new ResourceManager(this);
+        externalStorage = new File(Environment.getExternalStorageDirectory(), getPackageName());
+
+        // Figure out the directory where the game is. If the game was
+        // given to us via an intent, then we use the scheme-specific
+        // part of that intent to determine the file to launch. We
+        // also use the android.txt file to determine the orientation.
+        //
+        // Otherwise, we use the public data, if we have it, or the
+        // private data if we do not.
+        if (getIntent() != null && getIntent().getAction() != null &&
+                getIntent().getAction().equals("org.renpy.LAUNCH")) {
+            mPath = new File(getIntent().getData().getSchemeSpecificPart());
+        } else if (resourceManager.getString("public_version") != null) {
+            mPath = externalStorage;
+        } else {
+            mPath = getFilesDir();
+        }
+    }
+
+    /**
+     * Show an error using a toast. (Only makes sense from non-UI
+     * threads.)
+     */
+    public void toastError(final String msg) {
+
+        final Activity thisActivity = this;
+
+        runOnUiThread(new Runnable () {
+            public void run() {
+                Toast.makeText(thisActivity, msg, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Wait to show the error.
+        synchronized (this) {
+            try {
+                this.wait(1000);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    public void recursiveDelete(File f) {
+        if (f.isDirectory()) {
+            for (File r : f.listFiles()) {
+                recursiveDelete(r);
+            }
+        }
+        f.delete();
+    }
+
+
+    /**
+     * This determines if unpacking one the zip files included in
+     * the .apk is necessary. If it is, the zip file is unpacked.
+     */
+    public void unpackData(final String resource, File target) {
+
+        // The version of data in memory and on disk.
+        String data_version = resourceManager.getString(resource + "_version");
+        String disk_version = null;
+
+        // If no version, no unpacking is necessary.
+        if (data_version == null) {
+            return;
+        }
+
+        // Check the current disk version, if any.
+        String filesDir = target.getAbsolutePath();
+        String disk_version_fn = filesDir + "/" + resource + ".version";
+
+        try {
+            byte buf[] = new byte[64];
+            InputStream is = new FileInputStream(disk_version_fn);
+            int len = is.read(buf);
+            disk_version = new String(buf, 0, len);
+            is.close();
+        } catch (Exception e) {
+            disk_version = "";
+        }
+
+        // If the disk data is out of date, extract it and write the
+        // version file.
+        if (! data_version.equals(disk_version)) {
+            Log.v(TAG, "Extracting " + resource + " assets.");
+
+            recursiveDelete(target);
+            target.mkdirs();
+
+            AssetExtract ae = new AssetExtract(this);
+            if (!ae.extractTar(resource + ".mp3", target.getAbsolutePath())) {
+                toastError("Could not extract " + resource + " data.");
+            }
+
+            try {
+                // Write .nomedia.
+                new File(target, ".nomedia").createNewFile();
+
+                // Write version file.
+                FileOutputStream os = new FileOutputStream(disk_version_fn);
+                os.write(data_version.getBytes());
+                os.close();
+            } catch (Exception e) {
+                Log.w("python", e);
+            }
+        }
+
+    }
+
+    public void run() {
+
+        unpackData("private", getFilesDir());
+        unpackData("public", externalStorage);
+
+        System.loadLibrary("sdl");
+        System.loadLibrary("sdl_image");
+        System.loadLibrary("sdl_ttf");
+        System.loadLibrary("sdl_mixer");
+        System.loadLibrary("python2.7");
+        System.loadLibrary("application");
+        System.loadLibrary("sdl_main");
+
+        System.load(getFilesDir() + "/lib/python2.7/lib-dynload/_io.so");
+        System.load(getFilesDir() + "/lib/python2.7/lib-dynload/unicodedata.so");
+
+        try {
+            System.loadLibrary("sqlite3");
+            System.load(getFilesDir() + "/lib/python2.7/lib-dynload/_sqlite3.so");
+        } catch(UnsatisfiedLinkError e) {
+        }
+
+        try {
+            System.load(getFilesDir() + "/lib/python2.7/lib-dynload/_imaging.so");
+            System.load(getFilesDir() + "/lib/python2.7/lib-dynload/_imagingft.so");
+            System.load(getFilesDir() + "/lib/python2.7/lib-dynload/_imagingmath.so");
+        } catch(UnsatisfiedLinkError e) {
+        }
+
+	//Start MainActivity
+	Intent intent = new Intent(this,MainActivity.class);
+	startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        _isPaused = true;
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        _isPaused = false;
+
+        if (!mLaunchedThread) {
+            mLaunchedThread = true;
+            new Thread(this).start();
+        }
+    }
+
+    public boolean isPaused() {
+        return _isPaused;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, final KeyEvent event) {
+        
+            return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, final KeyEvent event) {
+            return super.onKeyUp(keyCode, event);
+       
+    }
+
+    public static void start_service(String serviceTitle, String serviceDescription,
+            String pythonServiceArgument) {
+        Intent serviceIntent = new Intent(PythonActivity.mActivity, PythonService.class);
+        String argument = PythonActivity.mActivity.getFilesDir().getAbsolutePath();
+        String filesDirectory = PythonActivity.mActivity.mPath.getAbsolutePath();
+        serviceIntent.putExtra("androidPrivate", argument);
+        serviceIntent.putExtra("androidArgument", filesDirectory);
+        serviceIntent.putExtra("pythonHome", argument);
+        serviceIntent.putExtra("pythonPath", argument + ":" + filesDirectory + "/lib");
+        serviceIntent.putExtra("serviceTitle", serviceTitle);
+        serviceIntent.putExtra("serviceDescription", serviceDescription);
+        serviceIntent.putExtra("pythonServiceArgument", pythonServiceArgument);
+        PythonActivity.mActivity.startService(serviceIntent);
+    }
+
+    public static void stop_service() {
+        Intent serviceIntent = new Intent(PythonActivity.mActivity, PythonService.class);
+        PythonActivity.mActivity.stopService(serviceIntent);
+    }
+
+    //----------------------------------------------------------------------------
+    // Listener interface for onNewIntent
+    //
+
+    public interface NewIntentListener {
+        void onNewIntent(Intent intent);
+    }
+
+    private List<NewIntentListener> newIntentListeners = null;
+
+    public void registerNewIntentListener(NewIntentListener listener) {
+        if ( this.newIntentListeners == null )
+            this.newIntentListeners = Collections.synchronizedList(new ArrayList<NewIntentListener>());
+        this.newIntentListeners.add(listener);
+    }
+
+    public void unregisterNewIntentListener(NewIntentListener listener) {
+        if ( this.newIntentListeners == null )
+            return;
+        this.newIntentListeners.remove(listener);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if ( this.newIntentListeners == null )
+            return;
+        synchronized ( this.newIntentListeners ) {
+            Iterator<NewIntentListener> iterator = this.newIntentListeners.iterator();
+            while ( iterator.hasNext() ) {
+                (iterator.next()).onNewIntent(intent);
+            }
+        }
+    }
+
+    //----------------------------------------------------------------------------
+    // Listener interface for onActivityResult
+    //
+
+    public interface ActivityResultListener {
+        void onActivityResult(int requestCode, int resultCode, Intent data);
+    }
+
+    private List<ActivityResultListener> activityResultListeners = null;
+
+    public void registerActivityResultListener(ActivityResultListener listener) {
+        if ( this.activityResultListeners == null )
+            this.activityResultListeners = Collections.synchronizedList(new ArrayList<ActivityResultListener>());
+        this.activityResultListeners.add(listener);
+    }
+
+    public void unregisterActivityResultListener(ActivityResultListener listener) {
+        if ( this.activityResultListeners == null )
+            return;
+        this.activityResultListeners.remove(listener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if ( this.activityResultListeners == null )
+            return;
+        synchronized ( this.activityResultListeners ) {
+            Iterator<ActivityResultListener> iterator = this.activityResultListeners.iterator();
+            while ( iterator.hasNext() )
+                (iterator.next()).onActivityResult(requestCode, resultCode, intent);
+        }
+    }
+}
+

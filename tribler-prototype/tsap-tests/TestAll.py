b846f289e2717a22ad8857cd54df30df8a6516c2
wtud <ws+tud@dosoftware.nl>
2014-06-10 11:37:03 +0200

    Refactor TestAll.py

--- tribler-prototype/tsap-tests/TestAll.py
+++ tribler-prototype/tsap-tests/TestAll.py
@@ -1,14 +1,24 @@
+# coding: utf-8
+# Written by Wendo Sabée
+# Automatically loads all tests from *_test.py files
+
 import unittest
 import os
 
-# add all tests here
-for file in os.listdir(os.getcwd()):
-    if file.endswith('_test.py'):
-        try:
-            modulename = file[:len(file) - 3]
-            exec("from %s import *" % modulename)
-            print "Imported tests from %s" % modulename
-        except:
-            pass
+def import_all_tests():
+    """
+    Import all tests from *_test.py files
+    :return: Nothing
+    """
+    for file in os.listdir(os.getcwd()):
+        if file.endswith('_test.py'):
+            try:
+                modulename = file[:len(file) - 3]
+                exec("from %s import *" % modulename)
+                print "Imported tests from %s" % modulename
+            except:
+                pass
 
-unittest.main()
+if __name__ == '__main__':
+    import_all_tests()
+    unittest.main()

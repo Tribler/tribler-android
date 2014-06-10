1e27f17b7d389963cde590c0f6217ae8ffd88304
wtud <ws+tud@dosoftware.nl>
2014-06-10 11:18:49 +0200

    Wrap the main python file in a class

--- tribler-prototype/tsap/service/main.py
+++ tribler-prototype/tsap/service/main.py
@@ -1,4 +1,7 @@
-__author__ = 'user'
+# coding: utf-8
+
+# Written by Wendo Sabée
+# The main class that loads the Tribler session, all managers and sets up a XML-RPC server
 
 import threading
 import time
@@ -26,33 +29,59 @@ from TriblerSession import TriblerSession
 from XMLRpc import XMLRPCServer
 
 
+class TSAP():
+    tribler = None
+    xmlrpc = None
+    dm = None
+    tm = None
+    cm = None
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        """
+        This sets up a Tribler session, loads the managers and the XML-RPC server.
+        :return: Nothing.
+        """
+
+        _logger.error("Loading TriblerSessionService")
+        self.tribler = TriblerSession()
+        self.tribler.start_session()
+
+        _logger.error("Loading XMLRPCServer")
+        self.xmlrpc = XMLRPCServer(iface="0.0.0.0", port=8000)
+
+        _logger.error("Loading ChannelManager")
+        self.cm = ChannelManager(self.tribler.get_session(), self.xmlrpc)
+
+        _logger.error("Loading TorrentManager")
+        self.tm = TorrentManager(self.tribler.get_session(), self.xmlrpc)
+
+        _logger.error("Loading DownloadManager")
+        self.dm = DownloadManager(self.tribler.get_session(), self.xmlrpc)
+
+        _logger.error("Now running XMLRPC on http://%s:%s/tribler" % (self.xmlrpc._iface, self.xmlrpc._port))
+        self.xmlrpc.start_server()
+
+    def test_sintel(self):
+        """
+        Test the torrent download functionality by downloading sintel and starting a VLC stream.
+        :return: Nothing.
+        """
+        dl_sintel = self.dm.add_torrent(os.path.join(os.getcwdu(), "..", "data", "sintel.torrent"), os.path.join(os.getcwdu(), "downloaded_videos"))
+
+        if is_android(strict=True):
+            self.dm.launch_vlc(dl_sintel)
+
+        while True:
+            for dl in self.dm.get_downloads():
+                print self.dm._getDownload(dl)
+            time.sleep(5)
+
+
 if __name__ == '__main__':
-    _logger.error("Loading TriblerSessionService")
-    tribler = TriblerSession()
-    tribler.start_session()
+    tsap = TSAP()
+    tsap.run()
 
-    _logger.error("Loading XMLRPCServer")
-    xmlrpc = XMLRPCServer(iface="0.0.0.0", port=8000)
-
-    _logger.error("Loading ChannelManager")
-    cm = ChannelManager(tribler.get_session(), xmlrpc)
-
-    _logger.error("Loading TorrentManager")
-    tm = TorrentManager(tribler.get_session(), xmlrpc)
-
-    _logger.error("Loading DownloadManager")
-    dm = DownloadManager(tribler.get_session(), xmlrpc)
-
-    #dl_ad = dm.add_torrent(os.path.join(os.getcwdu(), "..", "data", "ad.torrent"), os.path.join(os.getcwdu(), "downloaded_videos"))
-    #dm.add_torrent(os.path.join(os.getcwdu(), "..", "data", "pone.torrent"), os.path.join(os.getcwdu(), "downloaded_videos"))
-
-    #if os.environ['ANDROID_HOST'].startswith("ANDROID"):
-    #    dm.launch_vlc(dl_ad)
-
-    #while True:
-    #    for dl in dm.get_downloads():
-    #        print dm._getDownload(dl)
-    #    time.sleep(5)
-
-    _logger.error("Now running XMLRPC on http://%s:%s/tribler" % (xmlrpc._iface, xmlrpc._port))
-    xmlrpc.start_server()
+    #tsap.test_sintel()

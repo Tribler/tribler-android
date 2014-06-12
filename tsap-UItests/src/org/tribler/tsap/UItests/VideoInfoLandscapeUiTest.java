564b1a9b8a9909478256fdb97df35263ebe50a1e
Niels Spruit <niellus16@hotmail.com>
2014-06-12 10:59:42 +0200

    VideoInfoLandscapeUiTest.testVideoInfoPlayButtonClick is now fixed by overriding this test

--- tsap-UItests/src/org/tribler/tsap/UItests/VideoInfoLandscapeUiTest.java
+++ tsap-UItests/src/org/tribler/tsap/UItests/VideoInfoLandscapeUiTest.java
@@ -2,7 +2,9 @@ package org.tribler.tsap.UItests;
 
 import android.os.RemoteException;
 
+import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiSelector;
 
 /**
  * UI test cases for the video info screen in landscape mode
@@ -22,5 +24,20 @@ public class VideoInfoLandscapeUiTest extends VideoInfoUiTest {
 			RemoteException {
 		super.startTSAP();
 		getUiDevice().setOrientationRight();
+	}
+
+	/**
+	 * Tests whether the 'Play video' Button behaves correctly when it is
+	 * pressed (=opens VLC). This test is actually the same as the one in
+	 * {@link VideoInfoUiTest}, but without override this test somehow doesn't
+	 * work
+	 * 
+	 * @throws RemoteException
+	 * @throws UiObjectNotFoundException
+	 */
+	@Override
+	public void testVideoInfoPlayButtonClick() throws RemoteException,
+			UiObjectNotFoundException {
+		super.testVideoInfoPlayButtonClick();
 	}
 }

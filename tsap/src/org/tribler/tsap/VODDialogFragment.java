be701b5e9c30cb2c01c1d381b6a9bda275ba9685
Niels Spruit <niellus16@hotmail.com>
2014-06-18 10:26:31 +0200

    Added basic fragment for a dialog

---
+++ tsap/src/org/tribler/tsap/VODDialogFragment.java
@@ -0,0 +1,27 @@
+package org.tribler.tsap;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class VODDialogFragment extends DialogFragment {
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// Use the Builder class for convenient dialog construction
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		builder.setMessage("Video starts playing in about ... seconds")
+				.setNegativeButton(R.string.cancel,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// User cancelled the dialog
+							}
+						});
+		
+		// Create the AlertDialog object and return it
+		return builder.create();
+	}
+
+}

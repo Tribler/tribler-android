8ed7f8998f2204929238a2d53bcb9f5f4b79e7d4
Schut <dirk_schut@live.nl>
2014-06-20 13:13:03 +0200

    Added support for returning the filtered torrent types.

--- tsap/src/org/tribler/tsap/settings/Settings.java
+++ tsap/src/org/tribler/tsap/settings/Settings.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class Settings {
 	public enum TorrentType {
@@ -26,6 +27,15 @@ public class Settings {
 	public static boolean getFamilyFilterOn() {
 		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(mContext);
 		return sharedPref.getBoolean("pref_familyFilter", true);
+	}
+	public static TorrentType getFilteredTorrentTypes() {
+		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(mContext);
+		String typeString = sharedPref.getString("pref_supportedTorrentTypes", "@string/pref_supported_types_default");
+		if (typeString.equals("Video"))
+			return TorrentType.VIDEO;
+		if (typeString.equals("All"))
+			return TorrentType.ALL;
+		return TorrentType.ALL;
 	}
 
 	public static void setup(URL url, Context context) {

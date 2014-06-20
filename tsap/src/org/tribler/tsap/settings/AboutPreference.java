fb9c37ad5b613c427683546ab55a944a69137eab
Niels Spruit <niellus16@hotmail.com>
2014-06-20 09:24:19 +0200

    Removed commented code

--- tsap/src/org/tribler/tsap/settings/AboutPreference.java
+++ tsap/src/org/tribler/tsap/settings/AboutPreference.java
@@ -9,27 +9,11 @@ public class AboutPreference extends DialogPreference {
 
 	public AboutPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
-//		setDialogLayoutResource(R.layout.pref_about);
-////		setDialogTitle(R.string.pref_os_licences);
-//		setPositiveButtonText(android.R.string.ok);
-//		setNegativeButtonText(android.R.string.cancel);
-//
-//		setDialogIcon(null);
-		
 	}
-	
+
 	@Override
-	public void showDialog(Bundle state)
-	{
-//		super.showDialog(state);
+	public void showDialog(Bundle state) {
 		AboutDialog dialog = new AboutDialog(getContext());
 		dialog.show();
 	}
-	
-//	@Override
-//	public View onCreateDialogView()
-//	{
-//		setDia
-//		return null;
-//	}
 }

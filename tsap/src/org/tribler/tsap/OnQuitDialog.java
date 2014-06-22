42d1a8cd9d5f05ccca47ed4d78ec32178787cd09
Niels Spruit <niellus16@hotmail.com>
2014-06-22 15:16:25 +0200

    Refactored OnQuitDialog class to implement the Observer interface

--- tsap/src/org/tribler/tsap/OnQuitDialog.java
+++ tsap/src/org/tribler/tsap/OnQuitDialog.java
@@ -1,5 +1,8 @@
 package org.tribler.tsap;
 
+import java.util.Observable;
+import java.util.Observer;
+
 import org.renpy.android.PythonService;
 
 import android.app.AlertDialog;
@@ -8,12 +11,14 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 
-public class OnQuitDialog extends AlertDialog implements OnClickListener {
+public class OnQuitDialog extends AlertDialog implements OnClickListener,
+		Observer {
 	private Context mContext;
 
 	public OnQuitDialog(Context context) {
 		super(context);
 		mContext = context;
+		setTitle("Quit");
 		setMessage("Are you sure you want to close this app including the Tribler service?");
 		setButton(BUTTON_POSITIVE, "Yes", this);
 		setButton(BUTTON_NEGATIVE, "No", this);
@@ -22,13 +27,23 @@ public class OnQuitDialog extends AlertDialog implements OnClickListener {
 	@Override
 	public void onClick(DialogInterface arg0, int buttonClicked) {
 		if (buttonClicked == BUTTON_POSITIVE) {
-			Intent startMain = new Intent(Intent.ACTION_MAIN);
-			startMain.addCategory(Intent.CATEGORY_HOME);
-			startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			mContext.startActivity(startMain);
-			PythonService.stop();
-			((MainActivity) mContext).finish();
+			// perform XML-RPC call to shutdown tribler (should wait 5 seconds
+			// before notifying this dialog)
+			new AlertDialog.Builder(mContext)
+					.setMessage("Shutting down Tribler...")
+					.setCancelable(false).setTitle("Quit").show();
 		}
+	}
+
+	// gets called when xml-rpc shutdown call returns, shuts down the app
+	@Override
+	public void update(Observable observable, Object data) {
+		Intent startMain = new Intent(Intent.ACTION_MAIN);
+		startMain.addCategory(Intent.CATEGORY_HOME);
+		startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		mContext.startActivity(startMain);
+		PythonService.stop();
+		((MainActivity) mContext).finish();
 	}
 
 }

6bcd5e66312c754e3de45cfe5cfa015f273997f8
wtud <ws+tud@dosoftware.nl>
2014-07-02 22:12:25 +0200

    Tests for the TORRENT_HEALTH.toColor() function

---
+++ tsap-tests/src/org/tribler/tsap/thumbgrid/tests/TorrentHealthTest.java
@@ -0,0 +1,41 @@
+package org.tribler.tsap.thumbgrid.tests;
+
+import java.io.Serializable;
+
+import org.tribler.tsap.Torrent;
+import org.tribler.tsap.thumbgrid.TORRENT_HEALTH;
+
+import android.graphics.Color;
+import android.test.AndroidTestCase;
+
+/**
+ * Unit tests for the TORRENT_HEALTH class
+ * 
+ * @author Wendo Sabée
+ */
+public class TorrentHealthTest extends AndroidTestCase {
+
+	public void testToColorUnknown()
+	{
+		TORRENT_HEALTH health = TORRENT_HEALTH.UNKNOWN;
+		assertEquals("TORRENT_HEALTH.UNKNOWN color incorrect", TORRENT_HEALTH.toColor(health), Color.GRAY);
+	}
+	
+	public void testToColorRed()
+	{
+		TORRENT_HEALTH health = TORRENT_HEALTH.RED;
+		assertEquals("TORRENT_HEALTH.RED color incorrect", TORRENT_HEALTH.toColor(health), Color.RED);
+	}
+
+	public void testToColorYellow()
+	{
+		TORRENT_HEALTH health = TORRENT_HEALTH.YELLOW;
+		assertEquals("TORRENT_HEALTH.YELLOW color incorrect", TORRENT_HEALTH.toColor(health), Color.YELLOW);
+	}
+
+	public void testToColorGreen()
+	{
+		TORRENT_HEALTH health = TORRENT_HEALTH.GREEN;
+		assertEquals("TORRENT_HEALTH.GREEN color incorrect", TORRENT_HEALTH.toColor(health), Color.GREEN);
+	}
+}

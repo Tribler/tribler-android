a164496b3688367b1ae011d9b2abee73f42ac1b6
Niels Spruit <niellus16@hotmail.com>
2014-07-02 09:45:40 +0200

    Changed DownloadListFragment

--- tsap/src/org/tribler/tsap/downloads/DownloadListFragment.java
+++ tsap/src/org/tribler/tsap/downloads/DownloadListFragment.java
@@ -2,6 +2,7 @@ package org.tribler.tsap.downloads;
 
 import java.io.Serializable;
 
+import org.tribler.tsap.R;
 import org.tribler.tsap.XMLRPC.XMLRPCConnection;
 import org.tribler.tsap.XMLRPC.XMLRPCConnection.IConnectionListener;
 import org.tribler.tsap.util.Poller;
@@ -9,13 +10,20 @@ import org.tribler.tsap.util.Poller;
 import android.app.ListFragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class DownloadListFragment extends ListFragment implements IConnectionListener {
+public class DownloadListFragment extends ListFragment implements
+		IConnectionListener {
 
 	private XMLRPCConnection mConnection;
 	private Poller mPoller;
+	private TextView mEmptyTextView;
+	public final static long POLLING_INTERVAL = 2000;
+
 	/**
 	 * Initializes the channel adapter
 	 * 
@@ -27,26 +35,34 @@ public class DownloadListFragment extends ListFragment implements IConnectionLis
 		super.onCreate(savedInstanceState);
 		setHasOptionsMenu(true);
 		this.setListAdapter(XMLRPCDownloadManager.getInstance().getAdapter());
-		mPoller = new Poller(XMLRPCDownloadManager.getInstance(), 2000);
+		mPoller = new Poller(XMLRPCDownloadManager.getInstance(),
+				POLLING_INTERVAL);
 		mConnection = XMLRPCConnection.getInstance();
 	}
-	
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_download_list, null);
+		mEmptyTextView = (TextView) v.findViewById(android.R.id.empty);
+		mEmptyTextView.setText(R.string.connection_loading);
+		return v;
+	}
+
 	/**
 	 * Starts polling
 	 */
 	@Override
-	public void onResume()
-	{
+	public void onResume() {
 		super.onResume();
 		mConnection.addListener(this);
 	}
-	
+
 	/**
 	 * Pauses polling
 	 */
 	@Override
-	public void onPause()
-	{
+	public void onPause() {
 		super.onPause();
 		mConnection.removeListener(this);
 		mPoller.stop();
@@ -67,17 +83,23 @@ public class DownloadListFragment extends ListFragment implements IConnectionLis
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		// Launching new Activity on tapping a single List Item
-		Intent i = new Intent(getActivity().getApplicationContext(), DownloadActivity.class);
-		i.putExtra(DownloadActivity.INTENT_MESSAGE, (Serializable) ((DownloadListAdapter) getListAdapter()).getItem(position));
+		Intent i = new Intent(getActivity().getApplicationContext(),
+				DownloadActivity.class);
+		i.putExtra(DownloadActivity.INTENT_MESSAGE,
+				(Serializable) ((DownloadListAdapter) getListAdapter())
+						.getItem(position));
 		startActivity(i);
 	}
 
 	@Override
 	public void onConnectionEstablished() {
+		mEmptyTextView.setText(R.string.download_list_empty);
 		mPoller.start();
 	}
+
 	@Override
 	public void onConnectionLost() {
+		mEmptyTextView.setText(R.string.connection_failed);
 		mPoller.stop();
 	}
 }

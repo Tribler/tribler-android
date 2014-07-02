3b6a067e8776844e4b86d0f7d047b2fa84b182fe
Schut <dirk_schut@live.nl>
2014-07-02 15:28:10 +0200

    Added test for XMLRPCThumbFolderManager

--- tsap-tests/src/org/tribler/tsap/XMLRPC/tests/StubbedXMLRPCConnection.java
+++ tsap-tests/src/org/tribler/tsap/XMLRPC/tests/StubbedXMLRPCConnection.java
@@ -4,15 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.tribler.tsap.XMLRPC.XMLRPCConnection;
+import org.tribler.tsap.XMLRPC.XMLRPCConnection.IConnectionListener;
 
-class StubbedXMLRPCConnection extends XMLRPCConnection{
-	Map<String, Object> returnValues;
+public class StubbedXMLRPCConnection extends XMLRPCConnection{
+	private Map<String, Object> returnValues;
+	private static StubbedXMLRPCConnection mInstance = null;
+	
+	private StubbedXMLRPCConnection () {
+	}
+	
+	public static StubbedXMLRPCConnection getInstance() {
+		if (mInstance == null) {
+			mInstance = new StubbedXMLRPCConnection();
+		}
+		return mInstance;
+	}
 	
 	public void setupStub() {
 		returnValues = new HashMap<String, Object>();
 	}
+	public void clearReturnValues() {
+		returnValues.clear();
+	}
 	
 	public void addReturnValue(String functionName, Object returnValue) {
+		if(returnValues.containsKey(functionName)) {
+			returnValues.remove(functionName);
+		}
 		returnValues.put(functionName, returnValue);
 	}
 	
@@ -20,4 +38,6 @@ class StubbedXMLRPCConnection extends XMLRPCConnection{
 	public Object call(String functionName, Object... params) {
 		return returnValues.get(functionName);
 	}
+	
+	public void addListener(IConnectionListener listener) { }
 }

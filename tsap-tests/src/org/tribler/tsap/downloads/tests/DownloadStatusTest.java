af47d828a605611b2d860e887125a04f9117fe19
Niels Spruit <niellus16@hotmail.com>
2014-07-02 23:34:35 +0200

    org.tribler.tsap.downloads.tests ready for SIG

--- tsap-tests/src/org/tribler/tsap/downloads/tests/DownloadStatusTest.java
+++ tsap-tests/src/org/tribler/tsap/downloads/tests/DownloadStatusTest.java
@@ -5,8 +5,9 @@ import org.tribler.tsap.downloads.DownloadStatus;
 import junit.framework.TestCase;
 
 public class DownloadStatusTest extends TestCase {
-	public void testDownloadStatus () {
-		DownloadStatus downloadStatus = new DownloadStatus(3, 33.5, 129.8, 0.44, 47.1);
+	public void testDownloadStatus() {
+		DownloadStatus downloadStatus = new DownloadStatus(3, 33.5, 129.8,
+				0.44, 47.1);
 		assertEquals(3, downloadStatus.getStatus());
 		assertEquals(33.5, downloadStatus.getDownloadSpeed());
 		assertEquals(129.8, downloadStatus.getUploadSpeed());

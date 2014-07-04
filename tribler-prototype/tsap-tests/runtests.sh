1c610c6a4b9dd421a074fad49c51725450815575
wtud <ws+tud@dosoftware.nl>
2014-07-04 15:26:08 +0200

    Add 30s sleep to give Tribler time to start

--- tribler-prototype/tsap-tests/runtests.sh
+++ tribler-prototype/tsap-tests/runtests.sh
@@ -2,6 +2,8 @@ rm -rf runtests
 virtualenv --system-site-packages runtests
 . runtests/bin/activate
 pip install pytest
+#nosetests -v -m='*_test.py'
+sleep 30
 python TestAll.py
 deactivate
 rm -rf runtests
